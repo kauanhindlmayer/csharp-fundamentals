5191658a3945625e8034c15b63250c62a4496bea
Kauan <kauanhindlmayer07@gmail.com>
2022-10-26 14:32:50 -0300

    Moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      // Tipo para moedas
+      Console.Clear();
+
+      decimal valor = 10536.25m;
+
+      // Formatando moedas
+      Console.WriteLine(valor.ToString(
+        "C",
+        CultureInfo.CreateSpecificCulture("pt-BR")
+      ));
+
+      // Math
+      Console.WriteLine(
+        "Round: " + Math.Round(valor) + "\n" +
+        "Ceiling: " + Math.Ceiling(valor) + "\n" +
+        "Floor: " + Math.Floor(valor)
+      );
+    }
+  }
+}
